b0310f4eeb74308966da6ecfa8e82e35281865ab
kuritaeiji <eiji199825@yahoo.co.jp>
2022-02-28 11:33:43 +0900

    テスト

--- test/middleware/prepareDaysPage.spec.js
+++ test/middleware/prepareDaysPage.spec.js
@@ -1,10 +1,17 @@
 import prepareDaysPage from '~/middleware/prepareDaysPage'
 
 describe('middleware/prepareDaysPage', () => {
+  const mockAxios = { $post: jest.fn() }
+  const params = { date: '2022-01-01' }
+  const mockStore = { dispatch: jest.fn() }
+
   it('axiosは/api/v1/daysに$postする', () => {
-    const mockAxios = { $post: jest.fn() }
-    const params = { date: '2022-01-01' }
     prepareDaysPage({ $axios: mockAxios, params })
     expect(mockAxios.$post).toHaveBeenCalledWith('/api/v1/days', { day: params })
+  })
+
+  it('storeはcreateBodyをdispatchする', async () => {
+    await prepareDaysPage({ $axios: mockAxios, params, store: mockStore })
+    expect(mockStore.dispatch).toHaveBeenCalledWith('body/createBody', params.date)
   })
 })
