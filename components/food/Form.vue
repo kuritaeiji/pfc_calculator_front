1199f3f6bdea9a750335d8fe66f6a81fdbcd912b
kuritaeiji <eiji199825@yahoo.co.jp>
2022-03-10 11:42:20 +0900

    数値だけを入力可能なフォームにする

--- components/food/Form.vue
+++ components/food/Form.vue
@@ -65,9 +65,11 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import numberInput from '~/mixins/numberInput'
 import { decimal, presence, shorter } from '~/validators'
 
 export default {
+  mixins: [numberInput],
   props: {
     title: {
       type: String,
@@ -185,31 +187,6 @@ export default {
     },
     selectCategoryItems () {
       return this.categories.map(c => ({ text: c.title, value: c.id }))
-    },
-    lastCharIsCommaAndLastSecondCharIsNumber () {
-      return (string) => {
-        const lastChar = string.slice(-1)
-        const lastSecondChar = string.slice(-2)[0]
-        return lastChar === '.' && typeof Number(lastSecondChar) === 'number'
-      }
-    }
-  },
-  methods: {
-    setOnlyNumber (attrName, newVal) {
-      if (this.lastCharIsCommaAndLastSecondCharIsNumber(newVal)) {
-        this.$refs[attrName].lazyValue = newVal
-        this.$emit(`update:${attrName}`, newVal)
-        return null
-      }
-
-      const num = Number(newVal)
-      if (typeof num === 'number') {
-        this.$refs[attrName].lazyValue = num
-        this.$emit(`update:${attrName}`, num)
-        return null
-      }
-
-      this.$refs[attrName].lazyValue = this[attrName]
     }
   }
 }
